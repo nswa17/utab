f0448d987422631c89492316e4be29d8ebfca88f
nswa17 <nswa17@gmail.com>
2026-03-05 18:21:16 -0500

    Point web client at versioned API by default

--- packages/web/src/utils/api.ts
+++ packages/web/src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import type { Router } from 'vue-router'
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL ?? '/api',
+  baseURL: import.meta.env.VITE_API_URL ?? '/api/v1',
   withCredentials: true,
 })
 
