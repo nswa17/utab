5fae892819498f2d39da7531ec2e8fd8b6005d7b
Ryumei N <nswa17@gmail.com>
2026-09-19 14:57:03 -0400

    Restore deferred helper for submission race tests

--- packages/web/src/stores/submissions.test.ts
+++ packages/web/src/stores/submissions.test.ts
@@ -22,6 +22,16 @@ type MockedApi = {
 
 const mockedApi = api as unknown as MockedApi
 
+function createDeferred<T>() {
+  let resolve: (value: T) => void = () => {}
+  let reject: (reason?: unknown) => void = () => {}
+  const promise = new Promise<T>((res, rej) => {
+    resolve = res
+    reject = rej
+  })
+  return { promise, resolve, reject }
+}
+
 describe('submissions store', () => {
   beforeEach(() => {
     setActivePinia(createPinia())
