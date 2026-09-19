1b8fcaa6dccefec343620b492aed2609054eec51
Ryumei N <nswa17@gmail.com>
2026-09-19 14:14:18 -0400

    Preserve atomic break metadata saves in Phase 10

--- packages/web/src/views/admin/round/AdminRoundAllocation.vue
+++ packages/web/src/views/admin/round/AdminRoundAllocation.vue
@@ -2212,10 +2212,6 @@ async function syncAutoBreakPolicyToRound() {
   }
   const normalizedSize = normalizeBreakSize(autoBreakSize.value)
   autoBreakSize.value = normalizedSize
-  const currentUserDefined =
-    currentTournament.user_defined_data && typeof currentTournament.user_defined_data === 'object'
-      ? ({ ...(currentTournament.user_defined_data as Record<string, any>) } as Record<string, any>)
-      : {}
   const currentBreak = readTournamentBreakConfig()
   const breakConfig = {
     ...currentBreak,
@@ -2226,8 +2222,7 @@ async function syncAutoBreakPolicyToRound() {
   }
   const updated = await tournamentStore.updateTournament({
     tournamentId: currentTournament._id,
-    user_defined_data: {
-      ...currentUserDefined,
+    user_defined_data_patch: {
       break: breakConfig,
     },
   })
