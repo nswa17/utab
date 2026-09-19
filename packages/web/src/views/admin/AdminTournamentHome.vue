735a735df558e295cad818cff8a8e2e5ecc46179
Ryumei N <nswa17@gmail.com>
2026-09-18 22:49:13 -0400

    Reset round-break save state on tournament switch

--- packages/web/src/views/admin/AdminTournamentHome.vue
+++ packages/web/src/views/admin/AdminTournamentHome.vue
@@ -5040,6 +5040,7 @@ watch(
     isSavingTournamentBreak.value = false
     isSavingTournamentTeamRanking.value = false
     isSavingTournamentAdjudicatorRanking.value = false
+    setupRoundBreakUpdating.value = false
     tournamentAutosaveStatus.value = 'idle'
     tournamentAutosaveError.value = ''
     noticeSaveError.value = ''
