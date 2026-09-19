c4ff05eba8fad6b114688078cf8071c5636c2ae3
Ryumei N <nswa17@gmail.com>
2026-09-19 12:00:51 -0400

    Patch auto-break settings without overwriting tournament metadata

--- packages/web/src/views/admin/round/AdminRoundAllocation.vue
+++ packages/web/src/views/admin/round/AdminRoundAllocation.vue
@@ -2213,10 +2213,6 @@ async function syncAutoBreakPolicyToRound(currentTournamentId = tournamentId.val
   }
   const normalizedSize = normalizeBreakSize(autoBreakSize.value)
   autoBreakSize.value = normalizedSize
-  const currentUserDefined =
-    currentTournament.user_defined_data && typeof currentTournament.user_defined_data === 'object'
-      ? ({ ...(currentTournament.user_defined_data as Record<string, any>) } as Record<string, any>)
-      : {}
   const currentBreak = readTournamentBreakConfig()
   const breakConfig = {
     ...currentBreak,
@@ -2227,8 +2223,7 @@ async function syncAutoBreakPolicyToRound(currentTournamentId = tournamentId.val
   }
   const updated = await tournamentStore.updateTournament({
     tournamentId: currentTournamentId,
-    user_defined_data: {
-      ...currentUserDefined,
+    user_defined_data_patch: {
       break: breakConfig,
     },
   })
