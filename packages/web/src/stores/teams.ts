3e5fea4458f08db892f1e83a4ffc7cdc31757a87
Ryumei N <nswa17@gmail.com>
2026-09-20 22:43:11 -0400

    audit: dedupe delayed create responses

--- packages/web/src/stores/teams.ts
+++ packages/web/src/stores/teams.ts
@@ -60,7 +60,7 @@ export const useTeamsStore = defineStore('teams', () => {
       if (created) {
         if (tournamentScope.isScopeCurrent(scopeToken)) {
           tournamentScope.invalidateFetches(payload.tournamentId)
-          teams.value = [created, ...teams.value]
+          teams.value = [created, ...teams.value.filter((item) => item._id !== created._id)]
         }
       }
       return created
