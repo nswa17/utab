1d51c3c47701bbd46abdb4afabf0589d9e58e61d
Ryumei N <nswa17@gmail.com>
2026-09-20 22:48:27 -0400

    audit: preserve newer fetched entity over delayed create response

--- packages/web/src/stores/speakers.ts
+++ packages/web/src/stores/speakers.ts
@@ -60,7 +60,9 @@ export const useSpeakersStore = defineStore('speakers', () => {
       if (created) {
         if (tournamentScope.isScopeCurrent(scopeToken)) {
           tournamentScope.invalidateFetches(payload.tournamentId)
-          speakers.value = [created, ...speakers.value.filter((item) => item._id !== created._id)]
+          if (!speakers.value.some((item) => item._id === created._id)) {
+            speakers.value = [created, ...speakers.value]
+          }
         }
       }
       return created
